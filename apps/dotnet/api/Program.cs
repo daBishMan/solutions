c5649e44e12683353c9b6e809e9ca6744b40e63f
dabishman <jaybishtawi@outlook.com>
2022-04-21 13:07:24 -0600

    Clean program.cs

--- apps/dotnet/api/Program.cs
+++ apps/dotnet/api/Program.cs
@@ -8,14 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container, using the extension method.
 builder.Services.AddApplicationServices();
-
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
-
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<StoreContext>(x =>
   x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerDocumentation();
 
@@ -24,14 +20,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseSwaggerDocumentation();
-
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 // To Seed the Data
